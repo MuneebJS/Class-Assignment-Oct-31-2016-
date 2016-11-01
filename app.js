cc0b1e5a76a0378fe9b2c90c0206edd3d914ed2e
Muneeb Khan <Muneeb Khan>
2016-11-01 10:58:50 -0700

    hell

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
-//create three arrays consisting multiple same elements and then add three of them with unique array which never consist double element.
-function removeDuplicates(Arr) {
-    for (var i = 0; i < len; i++) {
-        var match = Arr.slice(i, i + 1);
-        for (var j = i + 1; j < len; j++) {
-            if (match == Arr[j]) {
-                Arr.splice(j--, 1);
-            }
-        }
-    }
-}
+// //create three arrays consisting multiple same elements and then add three of them with unique array which never consist double element.
+// function removeDuplicates(Arr) {
+//     for (var i = 0; i < len; i++) {
+//         var match = Arr.slice(i, i + 1);
+//         for (var j = i + 1; j < len; j++) {
+//             if (match == Arr[j]) {
+//                 Arr.splice(j--, 1);
+//             }
+//         }
+//     }
+// }
 
-var arr1 = ["Salt", "Sugar", "Long", "Water"];
-var arr2 = ["Water", "Apple", "Banana", "Mango"];
-var arr3 = ["Salt", "Carret", "Brinjal", "Patato"];
-var combStr = arr1.slice(0) + "," + arr2.slice(0) + "," + arr3.slice(0);
-var newArr = combStr.split(",");
-var finalArr = [];
-var len = newArr.length;
-removeDuplicates(newArr);
-alert(newArr);
+// var arr1 = ["Salt", "Sugar", "Long", "Water"];
+// var arr2 = ["Water", "Apple", "Banana", "Mango"];
+// var arr3 = ["Salt", "Carret", "Brinjal", "Patato"];
+// var combStr = arr1.slice(0) + "," + arr2.slice(0) + "," + arr3.slice(0);
+// var newArr = combStr.split(",");
+// var finalArr = [];
+// var len = newArr.length;
+// removeDuplicates(newArr);
+// alert(newArr);
 
-//This code is for table when user write a number in prompt following code give them a complete table of that number.
+// //This code is for table when user write a number in prompt following code give them a complete table of that number.
 
-function table(promptValue) {
-    for(var i = 1; i <= 10; i++) { 
-        var multi = promptValue * i;
-        var fullTable = document.write(promptValue +  " " + "*"  + " " +   " " + i  + " "  + "=" + " " + multi + " " + "<br>");
-    } 
-}
-var userInput = Number(prompt("enter a number"));
-table(userInput);
+// function table(promptValue) {
+//     for(var i = 1; i <= 10; i++) { 
+//         var multi = promptValue * i;
+//         var fullTable = document.write(promptValue +  " " + "*"  + " " +   " " + i  + " "  + "=" + " " + multi + " " + "<br>");
+//     } 
+// }
+// var userInput = Number(prompt("enter a number"));
+// table(userInput);
 
-
+// function diamondPattern(num) {
+// for(var i = num; i <= num; i++) {
+//     document.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + i + "<br>" + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + i + i + "<br>" + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + i + i + i + "<br>" + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + i + i + i + i
+//      + "<br>" + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + i + i + i + i + i +"<br>" + "&nbsp;&nbsp;&nbsp;&nbsp;" + i + i + i + i + i  + i + "<br>"
+//      + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + i + i + i + i + i + "<br>" + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + i + i + i + i +  "<br>"
+//       + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + i + i + i +  "<br>"+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + i + i + "<br>"
+//       + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + i + "<br>");
+//     }    
+// }
+// var numForPattern = prompt("Enter a number for pattern");
+// diamondPattern(numForPattern);  
+n = 9;
+for (var i = n;i > 0; i--) {
+    document.write(i);
+}   
